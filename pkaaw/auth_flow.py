a8763f2380c85707772b2bcfee8666a23d625f62
almartin82 <almartin@gmail.com>
2015-07-31 17:34:36 -0400

    linting, docstrings

--- pkaaw/auth_flow.py
+++ pkaaw/auth_flow.py
@@ -1,3 +1,4 @@
+"""oauth 1.0 flow for khan-api"""
 from __future__ import print_function, unicode_literals
 import requests_oauthlib
 import requests
@@ -5,27 +6,27 @@ import urlparse
 import webbrowser
 import yaml
 # package specific
-import constants
+import pkaaw.constants
 
 
 # default method for loading key and secret
 with open('../keys.yml', 'r') as f:
-    keys = yaml.load(f)
+    APP_KEYS = yaml.load(f)
 
 
-def get_request_tokens(consumer_key=keys['consumer_key'],
-                       consumer_secret=keys['consumer_secret']):
+def get_request_tokens(consumer_key=APP_KEYS['consumer_key'],
+                       consumer_secret=APP_KEYS['consumer_secret']):
     """starts the oauth dance.  returns an authorization url
     where the user can grant access."""
 
     # requests time out.  request token from service provider
     oauth = requests_oauthlib.OAuth1(client_key=consumer_key,
                                      client_secret=consumer_secret)
-    r = requests.post(url=constants.request_token_url, auth=oauth)
+    khan_resp = requests.post(url=pkaaw.constants.REQUEST_TOKEN_URL, auth=oauth)
 
     # http://stackoverflow.com/a/27458812
     # service provider issues request tokens
-    creds = urlparse.parse_qs(r.content)
+    creds = urlparse.parse_qs(khan_resp.content)
 
     # parse and return the request tokens
     request_tokens = {
@@ -38,20 +39,21 @@ def get_request_tokens(consumer_key=keys['consumer_key'],
 
 def make_auth_url(request_tokens):
     """builds the url where a user can authorize the application"""
-    auth_url = constants.authorization_url
+    auth_url = pkaaw.constants.AUTHORIZATION_URL
     auth_url = auth_url + '?oauth_token=' + request_tokens['oauth_token']
     return auth_url
 
 
-def direct_user_to_provider(request_tokens):
+def manual_user_to_provider(request_tokens):
+    """for manual flow, raises a browser for the user to login"""
     url = make_auth_url(request_tokens)
     webbrowser.open(url, new=0, autoraise=True)
 
 
-def manual_auth_flow(consumer_key=keys['consumer_key'],
-                     consumer_secret=keys['consumer_secret']):
+def manual_auth_flow(consumer_key=APP_KEYS['consumer_key'],
+                     consumer_secret=APP_KEYS['consumer_secret']):
     """wrapper around the oauth functions, for CLI/desktop use."""
 
     req_tokens = get_request_tokens(consumer_key, consumer_secret)
-    direct_user_to_provider(req_tokens)
+    manual_user_to_provider(req_tokens)
     return None
--- pkaaw/constants.py
+++ pkaaw/constants.py
@@ -1,2 +1,3 @@
-request_token_url = 'https://www.khanacademy.org/api/auth2/request_token'
-authorization_url = 'https://www.khanacademy.org/api/auth2/authorize'
+"""shared constants (urls, etc) used across the package"""
+REQUEST_TOKEN_URL = 'https://www.khanacademy.org/api/auth2/request_token'
+AUTHORIZATION_URL = 'https://www.khanacademy.org/api/auth2/authorize'
