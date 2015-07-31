70acad663a77075fe34deb72765a4cb13836dc97
almartin82 <almartin@gmail.com>
2015-07-31 17:00:52 -0400

    flake8 stuff

--- tests/helper.py
+++ tests/helper.py
@@ -1,8 +1,8 @@
 """test helper methods"""
 import unittest
-import os
 
-class pkaaw_test(unittest.TestCase):
+
+class pkaawTest(unittest.TestCase):
     def configure(self):
         self.consumer_key = 'CzQhjzHT3a5p5sAM'
         self.consumer_secret = 'Tbhk683HDPYDM9ea'
--- tests/test_oauth.py
+++ tests/test_oauth.py
@@ -1,12 +1,15 @@
 """tests for auth_flow functions."""
 
 from pkaaw.auth_flow import get_request_tokens
-from helper import pkaaw_test
+from helper import pkaawTest
 
-class request_tokensTest(pkaaw_test):
+
+class request_tokensTest(pkaawTest):
     def setUp(self):
         self.configure()
 
     def test_get_request_tokens(self):
-        request_tokens = get_request_tokens(self.consumer_key, self.consumer_secret)
-        self.assertEqual(request_tokens.keys(), [u'oauth_token_secret', u'oauth_token'])
+        request_tokens = get_request_tokens(self.consumer_key,
+                                            self.consumer_secret)
+        self.assertEqual(request_tokens.keys(),
+                         [u'oauth_token_secret', u'oauth_token'])
