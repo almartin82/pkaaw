9a124532a2a9bb39826b36d983be2055e3b61d8f
almartin82 <almartin@gmail.com>
2015-08-03 15:11:56 -0400

    new test on token function

--- tests/test_oauth.py
+++ tests/test_oauth.py
@@ -11,18 +11,17 @@ class RequestTokensTest(pkaawTest):
     def test_get_request_tokens(self):
         request_tokens = get_request_tokens(self.consumer_key,
                                             self.consumer_secret)
-        self.assertEqual(request_tokens.keys(),
-                         [u'oauth_token_secret', u'oauth_token'])
+        self.assertEqual(request_tokens.auth.client.client_key,
+                        self.consumer_key)
+        self.assertEqual(request_tokens.auth.client.client_secret,
+                        self.consumer_secret)
+        self.assertEqual(
+            request_tokens.__dict__.keys(),
+            ['cookies', 'stream', 'hooks', 'redirect_cache', 'auth',
+            'trust_env', 'headers', 'cert', 'params', '_client', 'verify',
+            'proxies', 'adapters', 'max_redirects']
+)
 
 
-class RequestTokensTest(pkaawTest):
-    def setUp(self):
-        self.configure()
-
-    def test_get_request_tokens(self):
-        request_tokens = get_request_tokens(self.consumer_key,
-                                            self.consumer_secret)
-        self.assertEqual(request_tokens.keys(),
-                         [u'oauth_token_secret', u'oauth_token'])
 
 #https://www.khanacademy.org/api/auth/default_callback?oauth_token_secret=7zjXGfCsWfHQDrGe&oauth_token=t5052446087970816
