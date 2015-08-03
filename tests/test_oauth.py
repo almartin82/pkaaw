e01670670d7a9b94fdd5b9093982f3f45b86bde0
almartin82 <almartin@gmail.com>
2015-08-03 15:34:14 -0400

    coach and student tests

--- tests/test_oauth.py
+++ tests/test_oauth.py
@@ -1,10 +1,10 @@
 """tests for auth_flow functions."""
 
 from pkaaw.auth_flow import get_request_tokens
-from helper import pkaawTest
+from helper import StudentTest, CoachTest
 
 
-class RequestTokensTest(pkaawTest):
+class StudentRequestTokensTest(StudentTest):
     def setUp(self):
         self.configure()
 
@@ -12,16 +12,31 @@ class RequestTokensTest(pkaawTest):
         request_tokens = get_request_tokens(self.consumer_key,
                                             self.consumer_secret)
         self.assertEqual(request_tokens.auth.client.client_key,
-                        self.consumer_key)
+                         self.consumer_key)
         self.assertEqual(request_tokens.auth.client.client_secret,
-                        self.consumer_secret)
+                         self.consumer_secret)
         self.assertEqual(
             request_tokens.__dict__.keys(),
             ['cookies', 'stream', 'hooks', 'redirect_cache', 'auth',
-            'trust_env', 'headers', 'cert', 'params', '_client', 'verify',
-            'proxies', 'adapters', 'max_redirects']
-)
+             'trust_env', 'headers', 'cert', 'params', '_client', 'verify',
+             'proxies', 'adapters', 'max_redirects']
+        )
 
 
+class CoachRequestTokensTest(CoachTest):
+    def setUp(self):
+        self.configure()
 
-#https://www.khanacademy.org/api/auth/default_callback?oauth_token_secret=7zjXGfCsWfHQDrGe&oauth_token=t5052446087970816
+    def test_get_request_tokens(self):
+        request_tokens = get_request_tokens(self.consumer_key,
+                                            self.consumer_secret)
+        self.assertEqual(request_tokens.auth.client.client_key,
+                         self.consumer_key)
+        self.assertEqual(request_tokens.auth.client.client_secret,
+                         self.consumer_secret)
+        self.assertEqual(
+            request_tokens.__dict__.keys(),
+            ['cookies', 'stream', 'hooks', 'redirect_cache', 'auth',
+             'trust_env', 'headers', 'cert', 'params', '_client', 'verify',
+             'proxies', 'adapters', 'max_redirects']
+        )
