1599a0ca042bd18ff9592b0e0152de79e0ad6790
kimjam <jamesykim10@gmail.com>
2015-08-04 11:16:44 -0400

    changing tabs to spaces

--- tests/test_object.py
+++ tests/test_object.py
@@ -5,22 +5,22 @@ from helper import ObjectTest
 
 
 class ConsoleAuthTest(ObjectTest):
-	def setUp(self):
-		self.configure()
+    def setUp(self):
+        self.configure()
 
-	def test_get_request_tokens(self):
-		khan_auth = ConsoleAuth(self.APP_KEYS)
+    def test_get_request_tokens(self):
+        khan_auth = ConsoleAuth(self.APP_KEYS)
 
-		request_tokens = khan_auth.get_request_tokens()
+        request_tokens = khan_auth.get_request_tokens()
 
-		self.assertEqual(request_tokens.auth.client.client_key,
-						 self.APP_KEYS['consumer_key'])
+        self.assertEqual(request_tokens.auth.client.client_key,
+                         self.APP_KEYS['consumer_key'])
 
-		self.assertEqual(request_tokens.auth.client.client_secret,
-						 self.APP_KEYS['consumer_secret'])
-		self.assertEqual(
-			request_tokens.__dict__.keys(),
-			['cookies', 'stream', 'hooks', 'redirect_cache', 'auth',
-			 'trust_env', 'headers', 'cert', 'params', '_client', 'verify',
-			 'proxies', 'adapters', 'max_redirects']
-		)
+        self.assertEqual(request_tokens.auth.client.client_secret,
+                         self.APP_KEYS['consumer_secret'])
+        self.assertEqual(
+            request_tokens.__dict__.keys(),
+            ['cookies', 'stream', 'hooks', 'redirect_cache', 'auth',
+             'trust_env', 'headers', 'cert', 'params', '_client', 'verify',
+             'proxies', 'adapters', 'max_redirects']
+        )
