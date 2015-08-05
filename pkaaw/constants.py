5514af92ad3a173995949e99b27efdee9074d8e7
almartin82 <almartin@gmail.com>
2015-08-05 16:26:50 -0400

    request method

--- pkaaw/constants.py
+++ pkaaw/constants.py
@@ -2,3 +2,5 @@
 REQUEST_TOKEN_URL = 'https://www.khanacademy.org/api/auth2/request_token'
 AUTHORIZATION_URL = 'https://www.khanacademy.org/api/auth2/authorize'
 ACCESS_TOKEN_URL = 'https://www.khanacademy.org/api/auth2/access_token'
+STUDENTS_URL = 'http://www.khanacademy.org/api/v1/user/students'
+USER_URL = 'http://www.khanacademy.org/api/v1/user'
