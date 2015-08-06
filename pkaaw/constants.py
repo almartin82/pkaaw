99fa715303284be9cc1831d818886e5b758175be
kimjam <jamesykim10@gmail.com>
2015-08-06 12:26:55 -0400

    flake8...

--- pkaaw/constants.py
+++ pkaaw/constants.py
@@ -1,4 +1,5 @@
 """shared constants (urls, etc) used across the package"""
+# flake8: noqa
 REQUEST_TOKEN_URL = 'https://www.khanacademy.org/api/auth2/request_token'
 AUTHORIZATION_URL = 'https://www.khanacademy.org/api/auth2/authorize'
 ACCESS_TOKEN_URL = 'https://www.khanacademy.org/api/auth2/access_token'
