2fd7b5a2092b244945e7ccc56c9ae4357b8253a2
kimjam <jamesykim10@gmail.com>
2015-08-06 13:37:21 -0400

    updating tokens for coach and student

--- tests/helper.py
+++ tests/helper.py
@@ -13,13 +13,13 @@ class StudentTest(unittest.TestCase):
     def configure(self):
         self.consumer_key = 'CzQhjzHT3a5p5sAM'
         self.consumer_secret = 'Tbhk683HDPYDM9ea'
-        self.oauth_token = 't5638830775468032'
-        self.oauth_token_secret = 'QHrtkjnNJKxSYSqJ'
+        self.oauth_token = 't5693324045451264'
+        self.oauth_token_secret = 'rMdRdYZam4VqeJgF'
 
 
 class CoachTest(unittest.TestCase):
     def configure(self):
         self.consumer_key = 'CzQhjzHT3a5p5sAM'
         self.consumer_secret = 'Tbhk683HDPYDM9ea'
-        self.oauth_token = 't5008451446112256'
-        self.oauth_token_secret = 'TZhkUFtFmtSfma5J'
+        self.oauth_token = 't6529569076150272'
+        self.oauth_token_secret = 'MPJcjyPBZyUkbBf5'
