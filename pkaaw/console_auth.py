450ae011000b5e80c2c0fc68cf275f2f5355b189
kimjam <jamesykim10@gmail.com>
2015-08-06 09:31:28 -0400

    up to date console_auth

--- pkaaw/console_auth.py
+++ pkaaw/console_auth.py
@@ -1,7 +1,7 @@
-
 """oauth 1.0 flow for khan-api as a class"""
 from __future__ import print_function, unicode_literals
-import pkaaw.constants
+import requests_oauthlib
+import requests
 import pkaaw.auth_flow
 
 
@@ -13,3 +13,22 @@ class ConsoleAuth(object):
             self.consumer_key, self.consumer_secret)
         self.khan_auth = pkaaw.auth_flow.console_auth(self.khan_auth)
         self.tokens = pkaaw.auth_flow.fetch_access_token(self.khan_auth)
+        self.access_token = self.tokens['access_token']
+        self.access_token_secret = self.tokens['access_token_secret']
+
+        # oauth1 session to access resources
+        self.oauth = requests_oauthlib.OAuth1(
+            client_key=self.consumer_key,
+            client_secret=self.consumer_secret,
+            resource_owner_key=self.access_token,
+            resource_owner_secret=self.access_token_secret,
+            signature_type='auth_header',
+            signature_method='PLAINTEXT'
+        )
+
+    def get_oauth(self):
+        return self.oauth
+
+    def make_request(self, target_url):
+        """makes an authenticated request to the target url"""
+        requests.get(url=target_url, auth=self.oauth).json()
